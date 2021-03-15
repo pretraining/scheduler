4da64e67f810be17e448c69d7d59bcec8cda5e98
bzantium <ryumin93@gmail.com>
2021-03-15 11:47:59 +0900

    [mixed] feat: Add members and team leader - 증원된 멤버 포함 15명 작성 - requirements에 색상 표시를 위한 termcolor 입력

--- ladder.py
+++ ladder.py
@@ -1,16 +1,26 @@
 import random
+from termcolor import colored
 
-MEMBERS = ['고우영', '김보섭', '김형석', '김형준', '류민호', '류인제', '문영기', '양승무', '유원준', '최정', '최태균', '황동현']
-TEAMS = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3]
+
+MEMBERS = ['강재호', '고우영', '김대규', '김보섭', '김형석', 
+           '김형준', '류민호', '류인제', '문영기', '백혜림',
+           '양승무', '유원준', '최정', '최태균', '황동현']
+TEAMS = [1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3]
+
+
+assert len(MEMBERS) == len(TEAMS)
 
 def assign(members, teams):
     random.shuffle(members)
-    print("************")
+    print("**************")
     for i, (member, team) in enumerate(zip(members, teams)):
-        print("%s\t-> %d" % (member, team))
-        if (i + 1) % 4 == 0:
-            print("------------")
-    print("************")
+        if i % 5 == 0:
+            print(colored("%s\t-> t%d (team leader)" % (member, team), 'yellow'))
+        else:
+            print("%s\t-> t%d" % (member, team))
+        if (i + 1) % 5 == 0 and i < len(MEMBERS) - 1:
+            print("--------------")
+    print("**************")
 
 if __name__ == "__main__":
     assign(MEMBERS, TEAMS)
